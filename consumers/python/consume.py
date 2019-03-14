2a7cc9c9df2ceae0df5aff7ba9b2102296f54783
Jeremy Mills <jmills@bespokemetrics.com>
2019-03-14 14:35:43 -0400

    chore(base-root): rebase master to empty

--- consumers/python/consume.py
+++
@@ -1,9 +0,0 @@
-from consumers.python.app.app import App
-from consumers.python.config.config import setConfig
-
-if __name__=="__main__":
-
-    print("Starting Rabbit App")
-    setConfig()
-    app = App()
-    app.startApp()
