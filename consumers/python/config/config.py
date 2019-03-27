c2316d76aef124604b7a5f13deb49b205dd2bd36
stamzid <stamzid@bespokemetrics.com>
2019-03-27 18:48:11 -0400

    Add addtional vhost and login for ellisdon testing

--- consumers/python/config/config.py
+++ consumers/python/config/config.py
@@ -1,11 +1,11 @@
 import os
 
 def setConfig():
-    os.environ["RABBIT_USER"] = "client"
+    os.environ["RABBIT_USER"] = "ellisdon"
     os.environ["RABBIT_PASS"] = "guest"
     os.environ["RABBIT_HOST"] = "rabbit"
     os.environ["RABBIT_PORT"] = "5672"
-    os.environ["RABBIT_VHOST"] = "/client"
-    os.environ["RABBIT_QUEUE"] = "client.dev.v1"
-    os.environ["RABBIT_ROUTING"] = "fanout"
-    os.environ["RABBIT_EXCHANGE"] = "client.fanout"
+    os.environ["RABBIT_VHOST"] = "/ellisdon"
+    os.environ["RABBIT_QUEUE"] = "ellisdon.dev.v1"
+    os.environ["RABBIT_ROUTING"] = "ellisdon.direct"
+    os.environ["RABBIT_EXCHANGE"] = "ellisdon_exchange"
